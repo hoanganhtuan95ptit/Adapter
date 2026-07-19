033c1312b1d1d7ecdaa6f7be48fc9aeb6b35f50b
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2026-07-19 14:05:55 +0700

    add fun onclickitem

--- adapter/src/main/java/com/simple/adapter/ViewItemAdapter.kt
+++ adapter/src/main/java/com/simple/adapter/ViewItemAdapter.kt
@@ -1,7 +1,9 @@
 package com.simple.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.simple.adapter.base.BaseBindingViewHolder
 
@@ -13,6 +15,13 @@ abstract class ViewItemAdapter<VI : ViewItem, VB : ViewBinding>() : ViewItemAdap
     override fun createViewHolder(parent: ViewGroup, viewType: Int): BaseBindingViewHolder<VB> {
 
         val viewHolder = BaseBindingViewHolder(createViewBinding(LayoutInflater.from(parent.context), parent, viewType), viewType)
+
+        viewHolder.itemView.setOnSafeClickListener {
+
+            val viewItem = (viewHolder.getViewItem() as? VI) ?: return@setOnSafeClickListener
+
+            onViewItemClick(viewItem)
+        }
 
         return viewHolder
     }
@@ -26,7 +35,27 @@ abstract class ViewItemAdapter<VI : ViewItem, VB : ViewBinding>() : ViewItemAdap
         onBindViewHolder(binding as VB, viewType, position, item as VI, payloads)
     }
 
+    open protected fun onViewItemClick(item: VI) {
+    }
 
-    open fun onBindViewHolder(binding: VB, viewType: Int, position: Int, item: VI, payloads: List<String>) {
+    open protected fun onBindViewHolder(binding: VB, viewType: Int, position: Int, item: VI, payloads: List<String>) {
+    }
+
+    private fun View.setOnSafeClickListener(onSafeClick: (View) -> Unit) {
+
+        var lastTimeClicked: Long = 0
+
+        setOnClickListener {
+
+            if (System.currentTimeMillis() - lastTimeClicked < 1000) return@setOnClickListener
+            lastTimeClicked = System.currentTimeMillis()
+
+            onSafeClick(it)
+        }
+    }
+
+    protected fun RecyclerView.ViewHolder.getViewItem(): ViewItem? {
+
+        return (bindingAdapter as? MultiAdapter)?.currentList?.getOrNull(absoluteAdapterPosition)
     }
 }
